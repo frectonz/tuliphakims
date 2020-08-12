bad2b290564ad64c1278de2ff22ec1c6dc91a591
fraol0912 <fraol0912@gmail.com>
2020-08-12 14:48:47 +0400

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,19 @@
+const navMenu = document.querySelector(".header-nav-menu > img");
+const fullscreenNav = document.querySelector(".fullscreen-nav");
+const fullscreenNavClose = document.querySelector(".fullscreen-nav__close");
+console.log(fullscreenNav);
+
+navMenu.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "auto"
+    });
+    fullscreenNav.style.display = "grid";
+    document.body.style.overflowY = "hidden";
+});
+
+fullscreenNavClose.addEventListener("click", () => {
+    fullscreenNav.style.display = "none";
+    document.body.style.overflowY = "scroll";
+});
